dab7ac02119c77d7ce05e3859f1811d67c46c0fb
YOGESHWARIR <105488124+YOGESHWARIR@users.noreply.github.com>
2023-05-15 14:13:31 +0530

    Added untracked files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Uncontrolled extends React.Component{
+  constructor(props){
+    super(props);
+    this.updateSubmit=this.updateSubmit.bind(this);
+    this.input=React.createRef();
+  }
+  updateSubmit(event){
+    alert('You have entered the username and college name successfully');
+    event.preventDefault();
+  }
+  render()
+  {
+    return(
+      <form onSubmit={this.updateSubmit}>
+        <h1>Uncontrolled form Example</h1>
+        <label>Name:<input type="text" ref={this.input}/></label>
+        <br></br>
+        <label>College Name:<input type="text" ref={this.input}/></label>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
 }
+export default Uncontrolled;
+
+
+
+
+
 
 export default App;
